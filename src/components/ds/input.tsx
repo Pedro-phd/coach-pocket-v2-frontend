c1bb17d9e725d74e9c0a68933e7e6dc270d0f040
nardini-22 <arthur.nardini03@gmail.com>
2024-07-13 20:56:44 -0300

    feat: add mask prop to input component

--- src/components/ds/input.tsx
+++ src/components/ds/input.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
-import { InputMask } from '@react-input/mask'
+import { useIMask } from 'react-imask'
+import { inputMasks } from './input-masks'
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	prefix?: string
 	suffix?: string
 	errorMessage?: string
 	hint?: string
-	mask?: string
+	mask?: 'phone' | 'zipcode' | 'date' | 'document'
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
 	({ className, type, prefix, suffix, errorMessage, hint, mask, ...props }, ref) => {
+		const { ref: iMaskRef } = useIMask(mask ? inputMasks[mask] : {})
 		return (
 			<div className="flex flex-col gap-1">
 				<div
@@ -25,7 +27,19 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 					{prefix && (
 						<div className="bg-muted text-muted-foreground py-1 px-3 border-r flex items-center">{prefix}</div>
 					)}
-					<InputMask {...props} mask={mask} className="py-1 px-3 w-full outline-none" type={type} ref={ref} />
+					<input
+						{...props}
+						className="py-1 px-3 w-full outline-none"
+						type={type}
+						ref={(domNode) => {
+							if (domNode) {
+								if (ref) {
+									;(ref as (instance: HTMLInputElement) => void)(domNode)
+								}
+								iMaskRef.current = domNode
+							}
+						}}
+					/>
 					{suffix && (
 						<div className="ml-auto bg-muted text-muted-foreground py-1 px-3 border-l flex items-center">{suffix}</div>
 					)}
