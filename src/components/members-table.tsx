008276c7ae80b50989a75852b9cf1eb37ae29bd7
nardini-22 <arthur.nardini03@gmail.com>
2024-07-13 21:33:49 -0300

    feat: add design system card component

--- src/components/members-table.tsx
+++ src/components/members-table.tsx
@@ -1,86 +1,66 @@
-import { Button } from "@/components/ds"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import { Link } from "lucide-react"
+import { Button, Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ds'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { Link } from 'lucide-react'
 
-export default function MembersTable(props: { className?: string}) {
-  return (
-    <Card className={props.className}>
-      <CardHeader className="px-7">
-        <CardTitle>Alunos</CardTitle>
-        <CardDescription>Lista de alunos cadastrados.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Nome</TableHead>
-              <TableHead className="hidden sm:table-cell">Ultima atualização</TableHead>
-              <TableHead className="hidden md:table-cell">Ficha</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
-                  liam@example.com
-                </div>
-              </TableCell>
-              <TableCell className="hidden sm:table-cell">10/10/2024</TableCell>
-              
-              <TableCell>
-                <Button size='icon' variant='link' className="text-brand">
-                  <Link  className="size-4"/>
-                </Button>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
-                  liam@example.com
-                </div>
-              </TableCell>
-              <TableCell className="hidden sm:table-cell">10/10/2024</TableCell>
-              
-              <TableCell>
-                <Button size='icon' variant='link' className="text-brand">
-                  <Link  className="size-4"/>
-                </Button>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
-                  liam@example.com
-                </div>
-              </TableCell>
-              <TableCell className="hidden sm:table-cell">10/10/2024</TableCell>
-              
-              <TableCell>
-                <Button size='icon' variant='link' className="text-brand">
-                  <Link  className="size-4"/>
-                </Button>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
-  )
+export default function MembersTable(props: { className?: string }) {
+	return (
+		<Card className={props.className}>
+			<CardHeader className="px-7">
+				<CardTitle>Alunos</CardTitle>
+				<CardDescription>Lista de alunos cadastrados.</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead>Nome</TableHead>
+							<TableHead className="hidden sm:table-cell">Ultima atualização</TableHead>
+							<TableHead className="hidden md:table-cell">Ficha</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						<TableRow>
+							<TableCell>
+								<div className="font-medium">Liam Johnson</div>
+								<div className="hidden text-sm text-muted-foreground md:inline">liam@example.com</div>
+							</TableCell>
+							<TableCell className="hidden sm:table-cell">10/10/2024</TableCell>
+
+							<TableCell>
+								<Button variant="link" className="text-brand">
+									<Link className="size-4" />
+								</Button>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell>
+								<div className="font-medium">Liam Johnson</div>
+								<div className="hidden text-sm text-muted-foreground md:inline">liam@example.com</div>
+							</TableCell>
+							<TableCell className="hidden sm:table-cell">10/10/2024</TableCell>
+
+							<TableCell>
+								<Button variant="link" className="text-brand">
+									<Link className="size-4" />
+								</Button>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell>
+								<div className="font-medium">Liam Johnson</div>
+								<div className="hidden text-sm text-muted-foreground md:inline">liam@example.com</div>
+							</TableCell>
+							<TableCell className="hidden sm:table-cell">10/10/2024</TableCell>
+
+							<TableCell>
+								<Button variant="link" className="text-brand">
+									<Link className="size-4" />
+								</Button>
+							</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</CardContent>
+		</Card>
+	)
 }
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,89 +1,87 @@
-"use client";
-import React, { useRef, useState } from "react";
-import useMeasure from "react-use-measure";
-import { motion, MotionConfig } from "framer-motion";
-import useClickOutside from "@/hooks/useClickOutside";
-import { ArrowLeft, Dumbbell, Gauge, Search, User } from "lucide-react";
-import Image from "next/image";
-
+'use client'
 import logo from '@/../assets/logos/logo-white-32x32.png'
-import { useRouter } from "next/navigation";
+import useClickOutside from '@/hooks/useClickOutside'
+import { MotionConfig, motion } from 'framer-motion'
+import { Dumbbell, Gauge, User } from 'lucide-react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import type React from 'react'
+import { useRef, useState } from 'react'
+import useMeasure from 'react-use-measure'
 
 const transition = {
-  type: "ease",
-  bounce: 0.4,
-  duration: 0.7,
-};
+	type: 'ease',
+	bounce: 0.4,
+	duration: 0.7,
+}
 
 function Button({
-  children,
-  onClick,
-  disabled,
-  ariaLabel,
+	children,
+	onClick,
+	disabled,
+	ariaLabel,
 }: {
-  children: React.ReactNode;
-  onClick?: () => void;
-  disabled?: boolean;
-  ariaLabel?: string;
+	children: React.ReactNode
+	onClick?: () => void
+	disabled?: boolean
+	ariaLabel?: string
 }) {
-  return (
-    <button
-      className="relative flex h-9 w-9 shrink-0 scale-100 select-none appearance-none items-center justify-center rounded-lg text-zinc-500 transition-colors hover:bg-zinc-100 hover:text-zinc-800 focus-visible:ring-2 active:scale-[0.98] disabled:pointer-events-none disabled:opacity-50"
-      type="button"
-      onClick={onClick}
-      disabled={disabled}
-      aria-label={ariaLabel}
-    >
-      {children}
-    </button>
-  );
+	return (
+		<button
+			className="relative flex h-9 w-9 shrink-0 scale-100 select-none appearance-none items-center justify-center rounded-lg text-zinc-500 transition-colors hover:bg-zinc-100 hover:text-zinc-800 focus-visible:ring-2 active:scale-[0.98] disabled:pointer-events-none disabled:opacity-50"
+			type="button"
+			onClick={onClick}
+			disabled={disabled}
+			aria-label={ariaLabel}
+		>
+			{children}
+		</button>
+	)
 }
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [contentRef, { width, height }] = useMeasure();
+	const [isOpen, setIsOpen] = useState(false)
+	const containerRef = useRef<HTMLDivElement>(null)
+	const [contentRef, { width, height }] = useMeasure()
 
-  const { push } = useRouter()
+	const { push } = useRouter()
 
-  useClickOutside(containerRef, () => {
-    setIsOpen(false);
-  });
+	useClickOutside(containerRef, () => {
+		setIsOpen(false)
+	})
 
-  return (
-    <MotionConfig transition={transition}>
-      <motion.div 
-        initial={{ y:20, opacity: 0 }}
-        whileInView={{ y:0, opacity: 1 }}
-        className="w-fit absolute bottom-9 right-0 left-0 ml-auto mr-auto" 
-        ref={containerRef}
-      >
-        <div className="h-full w-full rounded-xl border border-zinc-950/10 bg-white">
-          <motion.div
-            initial={false}
-          >
-            <div ref={contentRef} className="overflow-hidden p-2">
-                <div className="flex space-x-2">
-                  <button
-                    className="bg-brand relative flex h-9 w-9 shrink-0 scale-100 select-none appearance-none items-center justify-center rounded-lg text-zinc-500 transition-colors focus-visible:ring-2 active:scale-[0.98] disabled:pointer-events-none disabled:opacity-50"
-                    type="button"
-                  >
-                    <Image alt="logo coach pocket" src={logo} width={24} height={24} />
-                  </button>
-                  <Button ariaLabel="Dashboard" onClick={() => push('/')}>
-                    <Gauge className="h-5 w-5" />
-                  </Button>
-                  <Button ariaLabel="User members" onClick={() => push('/dashboard/members')}>
-                    <User className="h-5 w-5" />
-                  </Button>
-                  <Button ariaLabel="Workout">
-                    <Dumbbell className="h-5 w-5" />
-                  </Button>
-                </div>
-            </div>
-          </motion.div>
-        </div>
-      </motion.div>
-    </MotionConfig>
-  );
+	return (
+		<MotionConfig transition={transition}>
+			<motion.div
+				initial={{ y: 20, opacity: 0 }}
+				whileInView={{ y: 0, opacity: 1 }}
+				className="w-fit absolute bottom-9 right-0 left-0 ml-auto mr-auto"
+				ref={containerRef}
+			>
+				<div className="h-full w-full rounded-xl border border-zinc-950/10 bg-white">
+					<motion.div initial={false}>
+						<div ref={contentRef} className="overflow-hidden p-2">
+							<div className="flex space-x-2">
+								<button
+									className="bg-brand relative flex h-9 w-9 shrink-0 scale-100 select-none appearance-none items-center justify-center rounded-lg text-zinc-500 transition-colors focus-visible:ring-2 active:scale-[0.98] disabled:pointer-events-none disabled:opacity-50"
+									type="button"
+								>
+									<Image alt="logo coach pocket" src={logo} width={24} height={24} />
+								</button>
+								<Button ariaLabel="Dashboard" onClick={() => push('/')}>
+									<Gauge className="h-5 w-5" />
+								</Button>
+								<Button ariaLabel="User members" onClick={() => push('/dashboard/members')}>
+									<User className="h-5 w-5" />
+								</Button>
+								<Button ariaLabel="Workout">
+									<Dumbbell className="h-5 w-5" />
+								</Button>
+							</div>
+						</div>
+					</motion.div>
+				</div>
+			</motion.div>
+		</MotionConfig>
+	)
 }
