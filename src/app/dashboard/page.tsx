21d9f72693a30b68596d62d64571c7e131faddfc
nardini-22 <arthur.nardini03@gmail.com>
2024-08-06 22:58:23 -0300

    fix: try to adjust deploy #2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,20 @@
 'use client'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ds'
-import { DatePicker } from '@/components/ds/date-picket'
 import { MembersCountChart } from '@/components/members-count-chart'
 import MembersTable from '@/components/members-table'
 import { Separator } from '@/components/ui/separator'
 import { makeQuery } from '@/lib/make-query'
 import { useSuspenseQuery } from '@tanstack/react-query'
 
+export const maxDuration = 60
+
 export default function DashboardPage() {
 	const { data } = useSuspenseQuery(makeQuery({ key: 'members', path: '/members' }))
 	return (
-		<div className='flex flex-col gap-4 md:gap-6'>
+		<div className="flex flex-col gap-4 md:gap-6">
 			<div>
-				<h1 className="text-zinc-600 font-semibold text-sm">
-					Seja bem-vindo
-				</h1>
-				<h1 className="text-zinc-800 font-semibold text-3xl">
-					Pedro
-				</h1>
+				<h1 className="text-zinc-600 font-semibold text-sm">Seja bem-vindo</h1>
+				<h1 className="text-zinc-800 font-semibold text-3xl">Pedro</h1>
 			</div>
 			<Separator />
 			<div className="lg:grid lg:grid-cols-6 gap-4 flex flex-col-reverse">
